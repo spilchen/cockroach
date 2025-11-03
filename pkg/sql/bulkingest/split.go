d561fe2e8d9fb730c383be8b68fd0d72ec6a024b
Matt Spilchen <matt.spilchen@cockroachlabs.com>
2025-11-03 15:56:59 -0400

    first drop

---
+++ pkg/sql/bulkingest/split.go
@@ -0,0 +1,38 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package bulkingest
+
+import (
+	"context"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/kv"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/roachpb"
+)
+
+func splitAndScatterSpans(ctx context.Context, db *kv.DB, spans []roachpb.Span) error {
+	// TODO(jeffswenson): when distributing work, we should re-scatter ranges if
+	// there are a small number of nodes with remaining work.
+	for _, span := range spans {
+		expirationTime := db.Clock().Now().Add(time.Hour.Nanoseconds(), 0)
+		if err := db.AdminSplit(ctx, span.Key, expirationTime); err != nil {
+			return err
+		}
+	}
+	for _, span := range spans {
+		req := &kvpb.AdminScatterRequest{
+			// Randomly distributed ranges to make ingest more balanced.
+			RequestHeader:   kvpb.RequestHeaderFromSpan(span),
+			RandomizeLeases: true,
+			MaxSize:         1,
+		}
+		// Tolerate scatter errors since they are not critical to correctness.
+		//nolint:returncheck
+		_, _ = kv.SendWrapped(ctx, db.NonTransactionalSender(), req)
+	}
+	return nil
+}
